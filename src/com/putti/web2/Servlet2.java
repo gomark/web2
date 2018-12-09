4ee693b440eed7f7252df923adbabb7596b1beba
puttitang <puttitang@puttitang-macbookpro2.lan>
2018-12-09 08:04:32 +0700

    why not updated?

--- src/com/putti/web2/Servlet2.java
+++ src/com/putti/web2/Servlet2.java
@@ -27,7 +27,7 @@ public class Servlet2 extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Hey!! updated Servlet2:Served at: ").append(request.getContextPath());
+		response.getWriter().append("Hey-x updated Servlet2:Served at: ").append(request.getContextPath());
 	}
 
 	/**
