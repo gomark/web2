237c503ecd7b81d214c78cfa486d277beda8d9d9
puttitang <puttitang@172.20.10.3>
2019-05-24 16:33:47 +0700

    canary

--- src/com/putti/web2/Servlet2.java
+++ src/com/putti/web2/Servlet2.java
@@ -27,7 +27,7 @@ public class Servlet2 extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		long mil = System.currentTimeMillis();
-		response.getWriter().append("Hello SCG today..abc").append(request.getContextPath()).append(String.valueOf(mil));
+		response.getWriter().append("Hello SCG tomorrow..abc").append(request.getContextPath()).append(String.valueOf(mil));
 		
 		System.out.println("currentMil " + String.valueOf(mil));
 	}
